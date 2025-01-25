ac5d0a4d5e4c698b4205c552bf5697a00de40e99
Bruno Franklin <bruno.franklin@valtech.com>
2025-01-25 01:44:46 -0300

    Create Api Tests for Create a new random user

--- cypress/tests/api/api.cy.js
+++ cypress/tests/api/api.cy.js
@@ -6,3 +6,26 @@ describe('API Tests for Serverest - Public Endpoints', () => {
         });
     });
 });
+
+describe('API Tests for Serverest - Create User', () => {
+    it('Create a user with random data', () => {
+      const randomName = `Fulano ${Math.random().toString(36).substring(2, 10)}`;
+      const randomEmail = `email-${Math.random().toString(36).substring(2, 15)}@qa.com.br`;
+      const randomPassword = `senha-${Math.random().toString(36).substring(2, 10)}`;
+
+      const newUser = {
+        nome: randomName,
+        email: randomEmail,
+        password: randomPassword,
+        administrador: 'false'
+      };
+
+      cy.request({
+        method: 'POST',
+        url: 'https://serverest.dev/usuarios',
+        body: newUser
+      }).then((response) => {
+        expect(response.status).to.eq(201);
+      });
+    });
+  });
