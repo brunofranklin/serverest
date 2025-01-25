05a2a27ce6e5f89c0cb53c9f23ac086679f65181
Bruno Franklin <bruno.franklin@valtech.com>
2025-01-24 23:30:27 -0300

    Add RegisterPage

--- cypress/tests/e2e/e2e.cy.js
+++ cypress/tests/e2e/e2e.cy.js
@@ -1,3 +1,5 @@
+import RegisterPage from '../../support/pages/RegisterPage';
+
 describe('Test Login Page', () => {
 
     beforeEach(() => {
@@ -11,45 +13,14 @@ describe('Test Login Page', () => {
     });
 
     it('Check if the user registration is functional with random data', () => {
-      
-      const generateName = () => {
-        const firstNames = ['John', 'Maria', 'Carlos', 'Ana', 'Peter', 'Lucas'];
-        const lastNames = ['Smith', 'Santos', 'Johnson', 'Costa', 'Pereira'];
-        return `${firstNames[Math.floor(Math.random() * firstNames.length)]} ${lastNames[Math.floor(Math.random() * lastNames.length)]}`;
-      };
-  
-      const generateEmail = () => {
-        const letters = 'abcdefghijklmnopqrstuvwxyz';
-        const domain = 'test.com';
-        let email = '';
-        for (let i = 0; i < 10; i++) {
-          email += letters.charAt(Math.floor(Math.random() * letters.length));
-        }
-        return `${email}@${domain}`;
-      };
-  
-      const generatePassword = () => {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-        let password = '';
-        for (let i = 0; i < 8; i++) {
-          password += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-        return password;
-      };
-  
-      const name = generateName();
-      const email = generateEmail();
-      const password = generatePassword();
-  
-      cy.get('[data-testid="cadastrar"]').click();  
-      cy.get('[data-testid="nome"]').type(name);
-      cy.get('[data-testid="email"]').type(email);
-      cy.get('[data-testid="password"]').type(password);
-            
-      cy.get('[data-testid="cadastrar"]').click();
-      
-      cy.get('h1').contains('Serverest Store', { timeout: 10000 }).should('be.visible');
-  
+        const name = RegisterPage.generateName();
+        const email = RegisterPage.generateEmail();
+        const password = RegisterPage.generatePassword();
+    
+        RegisterPage.visit();
+        RegisterPage.fillRegistrationForm(name, email, password);
+        RegisterPage.submitRegistration();
+        RegisterPage.verifyRegistrationSuccess();
       });
 
 
